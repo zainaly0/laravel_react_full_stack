0e4381b769dd3d17b6b2f2da4b30d00415362ee2
zainaly0 <zaidk4076@gmail.com>
2024-09-16 14:32:14 +0530

    react routers with react router dom and createBrowserRouter

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,7 +1,26 @@
 import {createBrowserRouter} from 'react-router-dom'
+import Login from './viwes/Login';
+import Signup from './viwes/Signup';
+import Users from './viwes/Users';
+import NotFound from './viwes/NotFound';
 
 const router = createBrowserRouter([
-    
+   {
+    path: '/login',
+    element: <Login />
+   },
+   {
+    path: '/signup',
+    element: <Signup />
+   },
+   {
+    path: '/users',
+    element: <Users />
+   },
+   {
+    path: "*",
+    element: <NotFound />
+   }
 
 ])
 
