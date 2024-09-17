c58e66fddf1852b3ef22e8ceec93e614a4c59dd7
zainaly0 <zaidk4076@gmail.com>
2024-09-17 17:35:11 +0530

    Signup API is complete with Sanctum authentication token and Custorm validation

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,18 +6,27 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use App\Http\Requests\SignupRequest;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    public function login(LoginRequest $request){
-
-    }
-
-    public function signup(SignupRequest $request){
-
-    }
-
-    public function logout(Request $request){
+    public function login(LoginRequest $request) {
         
     }
+
+    public function signup(SignupRequest $request)
+    {
+        $data = $request->validated();
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response(compact('user', 'token'));
+    }
+
+    public function logout(Request $request) {}
 }
